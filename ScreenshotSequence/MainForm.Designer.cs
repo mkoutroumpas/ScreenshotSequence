7cde087872cff3a4ff6ec091c977c76724e233b8
Marios Koutroumpas <mkoutroumpas@gmail.com>
2019-03-31 11:09:26 +0200

    Initialized repository.

---
+++ ScreenshotSequence/MainForm.Designer.cs
@@ -0,0 +1,207 @@
+﻿using System.Windows.Forms;
+
+namespace ScreenshotSequence
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            _source?.Dispose();
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.nudInterval = new System.Windows.Forms.NumericUpDown();
+            this.lblInterval = new System.Windows.Forms.Label();
+            this.lblOutputDirectory = new System.Windows.Forms.Label();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.cbClearFolder = new System.Windows.Forms.CheckBox();
+            this.btnSelectFolder = new System.Windows.Forms.Button();
+            this.fbSelectFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.nudDuration = new System.Windows.Forms.NumericUpDown();
+            this.lblCaptureDuration = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudInterval)).BeginInit();
+            this.pnlMain.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDuration)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Location = new System.Drawing.Point(23, 154);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(286, 66);
+            this.btnStartStop.TabIndex = 0;
+            this.btnStartStop.Text = "Start (F11)";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.btnStartStop_Click);
+            // 
+            // nudInterval
+            // 
+            this.nudInterval.Increment = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nudInterval.Location = new System.Drawing.Point(220, 54);
+            this.nudInterval.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudInterval.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            65536});
+            this.nudInterval.Name = "nudInterval";
+            this.nudInterval.Size = new System.Drawing.Size(89, 20);
+            this.nudInterval.TabIndex = 1;
+            this.nudInterval.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // lblInterval
+            // 
+            this.lblInterval.AutoSize = true;
+            this.lblInterval.Location = new System.Drawing.Point(24, 56);
+            this.lblInterval.Name = "lblInterval";
+            this.lblInterval.Size = new System.Drawing.Size(133, 13);
+            this.lblInterval.TabIndex = 2;
+            this.lblInterval.Text = "Capture interval (seconds):";
+            // 
+            // lblOutputDirectory
+            // 
+            this.lblOutputDirectory.AutoSize = true;
+            this.lblOutputDirectory.Location = new System.Drawing.Point(24, 90);
+            this.lblOutputDirectory.Name = "lblOutputDirectory";
+            this.lblOutputDirectory.Size = new System.Drawing.Size(71, 13);
+            this.lblOutputDirectory.TabIndex = 3;
+            this.lblOutputDirectory.Text = "Output folder:";
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlMain.Controls.Add(this.nudDuration);
+            this.pnlMain.Controls.Add(this.lblCaptureDuration);
+            this.pnlMain.Controls.Add(this.cbClearFolder);
+            this.pnlMain.Controls.Add(this.btnSelectFolder);
+            this.pnlMain.Controls.Add(this.btnStartStop);
+            this.pnlMain.Controls.Add(this.lblOutputDirectory);
+            this.pnlMain.Controls.Add(this.nudInterval);
+            this.pnlMain.Controls.Add(this.lblInterval);
+            this.pnlMain.Location = new System.Drawing.Point(12, 12);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(329, 237);
+            this.pnlMain.TabIndex = 4;
+            // 
+            // cbClearFolder
+            // 
+            this.cbClearFolder.AutoSize = true;
+            this.cbClearFolder.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.cbClearFolder.Location = new System.Drawing.Point(23, 122);
+            this.cbClearFolder.Name = "cbClearFolder";
+            this.cbClearFolder.Size = new System.Drawing.Size(149, 17);
+            this.cbClearFolder.TabIndex = 6;
+            this.cbClearFolder.Text = "Empty selected folder first:";
+            this.cbClearFolder.UseVisualStyleBackColor = true;
+            // 
+            // btnSelectFolder
+            // 
+            this.btnSelectFolder.Location = new System.Drawing.Point(274, 85);
+            this.btnSelectFolder.Name = "btnSelectFolder";
+            this.btnSelectFolder.Size = new System.Drawing.Size(35, 22);
+            this.btnSelectFolder.TabIndex = 4;
+            this.btnSelectFolder.Text = "...";
+            this.btnSelectFolder.UseVisualStyleBackColor = true;
+            this.btnSelectFolder.Click += new System.EventHandler(this.btnSelectFolder_Click);
+            // 
+            // nudDuration
+            // 
+            this.nudDuration.Location = new System.Drawing.Point(220, 19);
+            this.nudDuration.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nudDuration.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.nudDuration.Name = "nudDuration";
+            this.nudDuration.Size = new System.Drawing.Size(89, 20);
+            this.nudDuration.TabIndex = 7;
+            this.nudDuration.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // lblCaptureDuration
+            // 
+            this.lblCaptureDuration.AutoSize = true;
+            this.lblCaptureDuration.Location = new System.Drawing.Point(24, 21);
+            this.lblCaptureDuration.Name = "lblCaptureDuration";
+            this.lblCaptureDuration.Size = new System.Drawing.Size(137, 13);
+            this.lblCaptureDuration.TabIndex = 8;
+            this.lblCaptureDuration.Text = "Capture duration (seconds):";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(349, 261);
+            this.Controls.Add(this.pnlMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Capture Screenshot Sequence";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.MainForm_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.nudInterval)).EndInit();
+            this.pnlMain.ResumeLayout(false);
+            this.pnlMain.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.nudDuration)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.NumericUpDown nudInterval;
+        private System.Windows.Forms.Label lblInterval;
+        private System.Windows.Forms.Label lblOutputDirectory;
+        private System.Windows.Forms.Panel pnlMain;
+        private System.Windows.Forms.FolderBrowserDialog fbSelectFolder;
+        private System.Windows.Forms.Button btnSelectFolder;
+        private System.Windows.Forms.CheckBox cbClearFolder;
+        private NumericUpDown nudDuration;
+        private Label lblCaptureDuration;
+    }
+}
+
---
+++ ScreenshotSequence/MainForm.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenshotSequence
+{
+    public partial class MainForm : Form
+    {
+        private string _folderPath = "";
+        private bool _isStarted = false;
+        private CancellationTokenSource _source = null;
+
+        #region Initialization
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            _source = new CancellationTokenSource((int)nudDuration.Value * 1000);
+
+            EnableControls(true);
+        }
+
+        private async void MainForm_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.F11)
+            {
+                await Start();
+            }
+            else if (e.KeyChar == (char)Keys.F12)
+            {
+                Stop();
+            }
+
+            e.Handled = true;
+        }
+
+        #endregion
+
+        #region UI interaction
+
+        private async void btnStartStop_Click(object sender, EventArgs e)
+        {
+            if (!_isStarted)
+            {
+                await Start();
+            }
+            else
+            {
+                Stop();
+            }
+        }
+
+        private void btnSelectFolder_Click(object sender, EventArgs e)
+        {
+            var dialogResult = fbSelectFolder.ShowDialog();
+            if (dialogResult == DialogResult.OK)
+            {
+                if (!string.IsNullOrEmpty(fbSelectFolder.SelectedPath))
+                {
+                    _folderPath = fbSelectFolder.SelectedPath;
+                    lblOutputDirectory.Text = _folderPath;
+                }
+            }
+        }
+
+        private void EnableControls(bool enable)
+        {
+            nudInterval.Enabled = enable;
+            nudDuration.Enabled = enable;
+            btnSelectFolder.Enabled = enable;
+            cbClearFolder.Enabled = enable;
+
+            btnStartStop.Text = enable ? "Start (F11)" : "Stop (F12)";
+        }
+
+        #endregion
+
+        #region Capture
+
+        private async Task Start()
+        {
+            EnableControls(false);
+
+            _isStarted = true;
+
+            await Task.Run(() => StartNewCaptureSequence((int)nudInterval.Value * 1000), _source.Token).ConfigureAwait(false);
+        }
+
+        private void Stop()
+        {
+            BreakCurrentCaptureSequence();
+
+            EnableControls(true);
+
+            _isStarted = false;
+        }
+
+        private void StartNewCaptureSequence(int intervalms)
+        {
+            if (_source == null)
+                return;
+
+            int cTicks = Environment.TickCount;
+
+            while (true)
+            {
+                if (_source.IsCancellationRequested)
+                {
+                    Stop();
+
+                    return;
+                }
+
+                if (Environment.TickCount == cTicks + intervalms)
+                {
+                    cTicks = Environment.TickCount;
+
+                    CaptureScreenshot();
+                }
+            }
+        }
+
+        private void CaptureScreenshot()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void BreakCurrentCaptureSequence()
+        {
+            _source?.Cancel();
+        }
+
+        #endregion
+    }
+}
