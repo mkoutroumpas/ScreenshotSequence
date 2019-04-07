351d79c867848a9f5a2c0a68d1fe9823f052ab5d
Marios Koutroumpas <mkoutroumpas@gmail.com>
2019-04-07 09:58:01 +0200

    Removed dispose.

--- ScreenshotSequence/MainForm.cs
+++ ScreenshotSequence/MainForm.cs
@@ -176,8 +176,6 @@ namespace ScreenshotSequence
             EnableControls(true);
 
             DumpImages(cbClearFolder.Checked);
-
-            _source.Dispose();
         }
 
         #endregion
